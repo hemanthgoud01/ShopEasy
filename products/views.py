48b177e94d009758addfb0424ef4a247930d6507
Hemanth <hemanthchinna0529@gmail.com>
2026-07-19 18:31:54 +0530

    Show template traceback

--- products/views.py
+++ products/views.py
@@ -3,14 +3,18 @@ from .models import Product
 
 
 from django.http import HttpResponse
-from .models import Product
+
+
+from django.shortcuts import render
 
 def home(request):
     try:
         products = Product.objects.all()
-        return HttpResponse(f"Products count: {products.count()}")
+        return render(request, "home.html", {"products": products})
     except Exception as e:
-        return HttpResponse(str(e))
+        from django.http import HttpResponse
+        import traceback
+        return HttpResponse(f"<pre>{traceback.format_exc()}</pre>")
 
 def product_detail(request, product_id):
     product = get_object_or_404(
