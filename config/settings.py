3a8bf4fbed2f5126397339e9dd281a59bf5ef070
Hemanth <hemanthchinna0529@gmail.com>
2026-07-19 19:10:39 +0530

    Add Render PostgreSQL configuration

--- config/settings.py
+++ config/settings.py
@@ -83,6 +83,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+import dj_database_url
+
 if os.environ.get("DATABASE_URL"):
     # Render PostgreSQL
     DATABASES = {
